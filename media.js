d76464f492a562f5f5dab993641a67f9812050be
dadaltsep <113356788+dadaltsep@users.noreply.github.com>
2023-09-25 11:46:34 -0300

    2509

--- media.js
+++ media.js
@@ -17,6 +17,10 @@ form.addEventListener("submit", (e) =>{
         resp2.innerText = `Parabéns ${nome} ! Voce foi aprovado (a)`
         resp2.style.color =  "blue"
     } else if (media >=4) {
-        
+     resp2.innerText = `Atenção ${nome}! Voce esta de exame`
+     resp2.style.color = "green"
+    }else{
+     resp2.innerText = `Ops ${nome}! Voce foi reprovado(a)`
+     resp2.style.color = "red"
     }
 })
